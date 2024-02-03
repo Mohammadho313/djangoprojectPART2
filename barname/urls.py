5b5ae95a06865dfba6cef2fa5fccbd0c0624205d
Sedamirmahdi <156710175+Sedamirmahdi@users.noreply.github.com>
2024-02-03 19:23:18 +0330

    Add files via upload

--- barname/urls.py
+++ barname/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create_clinic/', views.create_clinic_page),
     path('make_login/', views.make_login),
     path('make_signup/', views.make_signup),
-
+    path('make_add_clinic/', views.add_clinic),
+    path('panel/', views.panel),
     
--- barname/view.py
+++ barname/view.py
@@ -156,3 +156,78 @@ def make_signup(request):
         return redirect('/login')
 
 
+def add_clinic(request):
+    if request.method == 'POST':
+        clinic_id = request.POST['clinic_id']
+        name = request.POST['name']
+        email = request.POST['email']
+        address = request.POST['address']
+        phone_number = request.POST['phone_number']
+        services = request.POST['services']
+        try:
+            user = TheClinic.objects.get(email=email)
+            return redirect('/login?msg=ایمیل تکراری است')
+        except:
+            pass
+        try:
+            user = TheClinic.objects.get(clinic_id=clinic_id)
+            return redirect('/login?msg=آیدی کلینیک تکراری است')
+        except:
+            pass
+        clinic = TheClinic(clinic_id=clinic_id, email=email,name=name,address=address,phone_number=phone_number, services=services)
+        clinic.save()
+        return redirect('/panel/')
+    else:
+        return redirect('/login')
+
+def panel(request):
+    if 'username' not in request.session:
+        return redirect('../login/')
+    user = TheUser.objects.get(username=request.session['username'])
+    msg = request.GET.get('msg', "no_msg")
+    msg2 = msg.replace(" ","")
+    if(msg2 is None):
+        msg="no_msg"
+    if(msg == "no_msg"):
+        html = ""
+    else:
+        html = "<p>پیام سیستم: "+str(msg)+"</p>"
+    if(user.role == "بیمار"):
+        
+        html += """<html>   
+    <body>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css"/>
+<style>
+*{
+font-family: "Vazirmatn";
+direction: rtl;
+}
+</style>
+    <h1>پنل کاربری بیمار</h1><br>
+    <a href="/panel/new_appointment"><button>نوبت ویزیت جدید</button></a><br><br>
+    <a href="/panel/my_appointments"><button>نوبت های ویزیت من</button></a><br><br><br>
+    <a href="/panel/list_clinics"><button>مشاهده لیست کلینیک ها</button></a><br><br>
+    <a href="/panel/logout"><button>خروج از حساب کاربری</button></a>
+    </body>
+    </html>"""
+    if(user.role == "منشی"):
+        html = """<html>   
+    <body>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css"/>
+<style>
+*{
+font-family: "Vazirmatn";
+direction: rtl;
+}
+</style>
+    <h1>پنل کاربری منشی</h1><br>
+
+    <a href="/panel/pending_appointments"><button>نوبت های در انتظار تایید</button></a><br><br>
+    <a href="/panel/confirmed_appointments"><button>نوبت های تایید شده</button></a><br><br>
+        <a href="/panel/rejected_appointments"><button>نوبت های رد شده</button></a><br><br>
+    <a href="/panel/expired_appointments"><button>نوبت های گذشته</button></a><br><br>
+    
+    <a href="/panel/logout"><button>خروج از حساب کاربری</button></a>
+    </body>
+    </html>"""
+    return HttpResponse(html)
